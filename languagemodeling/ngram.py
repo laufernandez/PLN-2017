9e2f75993a52ae2c006561f8f2c192b148a55da7
Lautaro Fernandez Artico <fernandezarticolautaro@gmail.com>
2017-04-10 05:31:09 -0300

    Primeros avances en ngram.py.

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -14,9 +14,15 @@ class NGram(object):
         self.counts = counts = defaultdict(int)
 
         for sent in sents:
+            # Agrego n-1 delimitadores de inicio y uno de fin de sentencia
+            sent[0:0] = (n-1) * ['<s>']
+            sent.append('</s>')
+            # Creo el diccionario de n-gramas y (n-1)-gramas y sus frecuencias.
             for i in range(len(sent) - n + 1):
                 ngram = tuple(sent[i: i + n])
+                # Frecuencia de un n-grama en particular.
                 counts[ngram] += 1
+                # Frecuencia de cada (n-1) grama.
                 counts[ngram[:-1]] += 1
 
     def prob(self, token, prev_tokens=None):
@@ -27,3 +33,31 @@ class NGram(object):
 
         tokens = prev_tokens + [token]
         return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
+ 
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+        # Frecuencia asociada a la tupla que pasa como argumento.
+        return self.counts[tokens]
+
+ 
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+ 
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+ 
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+ 
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+ 
+        sent -- the sentence as a list of tokens.
+        """
