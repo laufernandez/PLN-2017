3aeeaad3febd9f1716d2b007f872ceac78697199
Lautaro Fernandez Artico <fernandezarticolautaro@gmail.com>
2017-04-13 19:53:56 -0300

    generate.py con flake8

---
+++ languagemodeling/scripts/generate.py
@@ -0,0 +1,33 @@
+"""
+Generate natural language sentences using a language model.
+
+Usage:
+  generate.py -i <file> -n <n>
+  generate.py -h | --help
+
+Options:
+  -i <file>     Language model file.
+  -n <n>        Number of sentences to generate.
+  -h --help     Show this screen.
+ """
+from docopt import docopt
+import pickle
+
+from languagemodeling.ngram import NGramGenerator
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+
+    # Cargo el modelo desde el archivo.
+    filename = opts['-i']
+    f = open(opts['-i'], 'rb')
+    n = int(opts['-n'])
+    model = pickle.load(f)
+    # Creo el ngrama.
+    ngram = NGramGenerator(model)
+    print("Caso " + str(ngram.n) + "-grama")
+    # Imprimo sentencia por sentencia
+    for i in range(n):
+        print("Sentencia " + str(i + 1) + (":"))
+        sent = ' '.join(ngram.generate_sent())
+        print(sent)
