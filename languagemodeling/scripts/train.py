43a0f85c3cd0bc33f0ae2e2d3cc5bb56428ccfbb
Lautaro Fernandez Artico <fernandezarticolautaro@gmail.com>
2017-04-16 01:58:25 -0300

    Nuevo corpus con tweets de politicos argentinos. Interfaz de train.py ahora permite elegir corpus.

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -2,7 +2,7 @@
 Train an n-gram model.
 
 Usage:
-  train.py -n <n> [-m <model>] -o <file>
+  train.py -n <n> [-m <model>] -o <file> [-c <corpus>]
   train.py -h | --help
 
 Options:
@@ -11,6 +11,9 @@ Options:
                   ngram: Unsmoothed n-grams.
                   addone: N-grams with add-one smoothing.
   -o <file>     Output model file.
+  -c <corpus>   Corpus to use [default: tweetspoliticos]:
+                  tweetspoliticos: Argentine Politicians Tweets.
+                  niunamenos: Ni Una Menos Tweets (MicaelaGarcia).
   -h --help     Show this screen.
 """
 
@@ -24,23 +27,30 @@ from corpus.twitter_corpus_reader import TwitterCorpusReader
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
-    # load the data
-    corpus = TwitterCorpusReader('../../corpus/', 'NiUnaMenos.txt')
-    sents = corpus.sents()
+    # Cargo el corpus segun seleccion del usuario.
+    c = opts['-c']
+    if c == 'tweetspoliticos':
+        txt_file = 'PoliticosArgentinos.txt'
+    elif c == 'niunamenos':
+        txt_file = 'NiUnaMenos.txt'
+    else:
+        __doc__
+    corpus = TwitterCorpusReader('../../corpus/', txt_file)
 
-    # train the model
-    n = int(opts['-n'])
-    # Si pasa algun argumento como opcion para el modelo.
+    # Entreno el modelo.
+    sents = corpus.sents()
+    n = int(opts['-n'])  # Orden
     m = opts['-m']  # Por default es 'ngram'.
     if m == 'addone':
-        print('Training an AddOneNgram Model')
+        print('Training an AddOneN{}-gram Model'.format(n))
         model = AddOneNGram(n, sents)
     elif m == 'ngram':
-        print('Training an Ngram Model')
+        print('Training an N{}-gram Model'.format(n))
         model = NGram(n, sents)
     else:
         __doc__
-    # save it
+
+    # Guardo los modelos en archivos binarios.
     filename = opts['-o']
     f = open(filename, 'wb')
     pickle.dump(model, f)
